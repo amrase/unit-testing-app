3f85578d725468e650b8574848e8cb5f7f027513
unknown <amrasezairi31@gmail.com>
2021-11-01 19:35:01 -0400

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
